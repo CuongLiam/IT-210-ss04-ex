818a820a4c372d2b522b40514c71e67ce877fa88
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-04-21 21:30:17 +0700

    up

---
+++ src/main/java/com/restaurant/controller/LegacyController.java
@@ -0,0 +1,12 @@
+package com.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LegacyController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "index"; // Sẽ trỏ về /WEB-INF/views/index.jsp
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Chào Liam! Spring MVC đã chạy thành công.</h2>
+</body>
+</html>
